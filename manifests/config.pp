b0ff3a6248909fa8cfbe360d59414f8ac98194d7
Frederik Wagner <wagner@wagit.de>
2014-08-04 08:36:50 +0200

    added additional scope tests

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,5 +1,20 @@
 #
 class test_scope::config {
+
+  file {"/tmp1/$testvalue":
+    ensure => present
+  }
+
+  $testval_tmpl = inline_template('<%= @testvalue -%>')
+  file {"/tmp2/$testval_tmpl":
+    ensure => present
+  }
+
+  $testvalue= $test_scope::testvalue
+  file {"/tmp3/$testvalue":
+    ensure => present
+  }
+
   file {'/tmp/test':
     content => template('test_scope/test.erb')
   }
